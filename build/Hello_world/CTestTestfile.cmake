f80feb8348a6c85a9a3ee1eaae97d667a42a592d
Mikhail <mfomenko123@ya.ru>
2018-12-31 13:43:30 +0700

    Test commit

---
+++ build/Hello_world/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mikhail/workspace/src/Hello_world
+# Build directory: /home/mikhail/workspace/build/Hello_world
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/mikhail/workspace/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/mikhail/workspace/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/mikhail/workspace/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/mikhail/workspace/src:$ROS_PACKAGE_PATH"
